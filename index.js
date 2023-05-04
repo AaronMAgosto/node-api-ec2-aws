5dddec00ad6547b0766fdcfca44466090be7a496
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-05-04 15:59:53 -0400

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => res.send("Welcome to my API"))
+
+const dogsIn = [
+  {
+  name: 'Paco',
+  breed:'chihuahua',
+  age: 33,
+  },
+  {
+  name: 'bob',
+  breed:'yorkie',
+  age: 34,
+  },
+  {
+  name: 'nimby',
+  breed:'pitbull',
+  age: 24,
+  },
+  {
+  name: 'aaron',
+  breed:'frenchie',
+  age: 34,
+  },
+]
+
+app.get('/', (req, res) => res.json(dogsIn))
 
 app.listen(4000, () => console.log(' api running on port 4000'))
